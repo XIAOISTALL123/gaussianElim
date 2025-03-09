b7ea3914147125e2fa91e24082e2142472e7aaaa
boyadik69 <4Zhiraphael@gmail.com>
2025-03-09 12:54:07 +0800

    function elim: disabled row multiplication if constant factor is 1

--- gaussian.py
+++ gaussian.py
@@ -64,7 +64,7 @@ def rearrangeRows(m: list, columnNumber: int, rowNumber: int) -> list:
 def elim(m: list):
     for i in range(1, len(m)+1):
         m = rearrangeRows(m, i, i)
-        if m[i-1][i-1] != 0:
+        if m[i-1][i-1] != 0 and m[i-1][i-1] != 1:
             m = multiply(m, i, 1/(m[i-1][i-1]))
         for j in range(i+1, len(m)+1):
             if -m[j-1][i-1] != 0:
